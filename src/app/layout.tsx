676ddb0cc5abea2f36c6b2ef600c028afff3398f
kwondns <kwondns@gmail.com>
2025-08-01 22:37:16 +0900

    💄 [Design]: RootLayout 컴포넌트 수정
    - RootLayout: body에 `h-dvh` 클래스 추가로 높이 계산 방식 개선
    - children 섹션에 padding과 높이 계산 추가로 레이아웃 정렬 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,10 @@ import Header from '@/templates/Header';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-background">
+      <body className="bg-background h-dvh">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Header />
-          {children}
+          <div className="px-2 h-[calc(100dvh-88px)]">{children}</div>
         </ThemeProvider>
       </body>
     </html>
