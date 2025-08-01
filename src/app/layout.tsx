8c1f990fee2ce31468061484fa8861c6e9b83ae2
kwondns <kwondns@gmail.com>
2025-08-01 18:58:16 +0900

    💄 [Style]: 기본 스타일 적용
    - shadcn theme 적용
    - dark mode 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import './globals.css';
+import { ThemeProvider } from '@/components/ui/theme-provider';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-background">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
