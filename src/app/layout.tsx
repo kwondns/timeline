93477e7db4a3f004df11fa39c9e491c6367bfea1
kwondns <kwondns@gmail.com>
2025-08-01 20:48:30 +0900

    ♻️ [Refactor]: suppressHydrationWarning 속성 추가
    - react 18 strict mode 환경에서 경고 제거 suppressHydrationWarning 속성 추가.
    - Next-theme 추가로 인한 조치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from '@/components/ui/theme-provider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="bg-background">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
