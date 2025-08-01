9bb74547a1b9bf3ce5ffaa4d364aacb4aa6d4de8
kwondns <kwondns@gmail.com>
2025-08-01 22:37:37 +0900

    💄 [Design]: Nav 컴포넌트 아이콘 버튼 스타일 수정
    - IconButton에 variant="outline" 속성 추가로 스타일 일관성 향상
    - '과거', '현재', '미래' 버튼 모두 동일한 스타일로 통일하여 UI 개선

--- src/organisms/Nav.tsx
+++ src/organisms/Nav.tsx
@@ -6,13 +6,13 @@ export default function Nav() {
   return (
     <Container className="gap-3">
       <TooltipButton context="과거">
-        <IconButton icon="sandClock" size="lg" />
+        <IconButton variant="outline" icon="sandClock" size="lg" />
       </TooltipButton>
       <TooltipButton context="현재">
-        <IconButton icon="time" size="lg" />
+        <IconButton variant="outline" icon="time" size="lg" />
       </TooltipButton>
       <TooltipButton context="미래">
-        <IconButton icon="rocket" size="lg" />
+        <IconButton variant="outline" icon="rocket" size="lg" />
       </TooltipButton>
     </Container>
   );
