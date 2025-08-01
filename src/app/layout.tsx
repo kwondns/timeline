4702a03982ae8416c905c403edd2c566847bf609
kwondns <kwondns@gmail.com>
2025-08-01 20:53:45 +0900

    🚀 [Feat]: Header RootLayout에 추가
    - Header 템플릿 컴포넌트를 RootLayout에 포함시켜 전체 페이지에 Header 적용.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import './globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
+import Header from '@/templates/Header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="bg-background">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Header />
           {children}
         </ThemeProvider>
       </body>
