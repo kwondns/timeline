42d5d55aad668c128d25f925827947c2fd80640f
kwondns <kwondns@gmail.com>
2025-08-02 17:01:13 +0900

    🚀 [Feat]: PresentInfo 컴포넌트 기능 및 스타일 개선
    - `calculateDateDiff` 유틸 함수 활용하여 날짜 차이 계산 로직 추가
    - Card 컴포넌트와 Typography 컴포넌트를 사용하여 UI 구조 개선
    - 진행 상태 표시 및 시작 날짜 표시 기능 추가
    - 기존 StatusBadgeGroup 및 KbdGroup 제거, 불필요한 코드 정리
    - `to`, `diff` Props 제거
    - `to`를 보여주는 곳이 없고 diff는 자체적으로 생성하여 제거

--- src/organisms/PresentInfo.tsx
+++ src/organisms/PresentInfo.tsx
@@ -1,21 +1,35 @@
+'use client';
+
 import { Input } from '@/components/ui/input';
-import StatusBadgeGroup, { StatusBadgeGroupProps } from '@/molecules/StatusBadgeGroup';
 import Container from '@/atoms/Container';
 import ActionButtonGroup, { ActionButtonGroupProps } from '@/molecules/ActionButtonGroup';
-import KbdGroup from '@/molecules/KbdGroup';
+import { Card } from '@/components/ui/card';
+import Typography from '@/atoms/Typography';
+import { calculateDateDiff } from '@/lib/date';
 
-type PresentInfoProps = StatusBadgeGroupProps & ActionButtonGroupProps;
+type PresentInfoProps = {
+  from: Date | null;
+} & ActionButtonGroupProps;
 
 export default function PresentInfo(props: PresentInfoProps) {
-  const { from, to, diff, onSave, onTempSave } = props;
+  const { from, onSave, onTempSave } = props;
+  const diff = calculateDateDiff(from ?? '');
+  const indicatorStyle =
+    "before:content-[''] before:w-3 before:h-3 before:rounded-full before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:bg-blue-300";
   return (
-    <Container direction="column" className="gap-4">
+    <Card className="gap-4 px-6 pt-6 bg-info-foreground/20 border-info/20">
+      <Container className="justify-between items-center">
+        <Typography.p className={`relative text-blue-300 px-5 ${indicatorStyle} ${from ? 'before:animate-pulse' : ''}`}>
+          {from ? '진행중' : '휴식중'}
+        </Typography.p>
+        <Typography.h4 className="text-primary/70">{from?.toLocaleString('ko-kr')}</Typography.h4>
+      </Container>
+      {/* TODO 기본 Typo, 더블클릭 시 Input으로 변경 */}
       <Input className="!text-2xl !py-6" id="title" placeholder="제목입력" />
-      <StatusBadgeGroup from={from} to={to} diff={diff} />
-      <Container className="justify-end gap-6">
-        <KbdGroup />
+      <Container className="justify-between gap-6 items-center">
+        <Typography.h2 className="!pb-0">{diff}</Typography.h2>
         <ActionButtonGroup onTempSave={onTempSave} onSave={onSave} />
       </Container>
-    </Container>
+    </Card>
   );
 }
