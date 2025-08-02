09e2d598afd8550fc207f9e5cfa65242abb5e4dc
kwondns <kwondns@gmail.com>
2025-08-02 17:02:12 +0900

    🚀 [Feat]: Header 제거 및 레이아웃 UI 수정
    - `Header` 컴포넌트 제거로 불필요한 코드 삭제 및 레이아웃 단순화
    - `px-2`를 `p-2`로 수정하여 여백 스타일 일관성 유지
    - `h-[calc(100dvh-88px)]`를 `h-dvh`로 변경해 높이 계산 최적화
    - Sidebar로 변경 예정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import './globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
-import Header from '@/templates/Header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="bg-background h-dvh">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Header />
-          <div className="px-2 h-[calc(100dvh-88px)]">{children}</div>
+          <div className="p-2 h-dvh">{children}</div>
         </ThemeProvider>
       </body>
     </html>
