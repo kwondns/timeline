5bec40a10c6865b4d742a50bdd431459fa11bf6d
kwondns <kwondns@gmail.com>
2025-08-02 16:01:20 +0900

    🚀 [Feat]: Nav 컴포넌트에 Link 추가로 페이지 이동 기능 구현
    - Nav 컴포넌트의 아이콘 버튼들을 Link 컴포넌트로 감싸 페이지 이동 기능 추가
      - '과거' 버튼 클릭 시 `/past`로 이동
      - '현재' 버튼 클릭 시 `/present`로 이동
      - '미래' 버튼 클릭 시 `/future`로 이동

--- src/organisms/Nav.tsx
+++ src/organisms/Nav.tsx
@@ -1,18 +1,25 @@
 import Container from '@/atoms/Container';
 import IconButton from '@/atoms/IconButton';
 import TooltipButton from '@/molecules/TooltipButton';
+import Link from 'next/link';
 
 export default function Nav() {
   return (
     <Container className="gap-3">
       <TooltipButton context="과거">
-        <IconButton variant="outline" icon="sandClock" size="lg" />
+        <Link href="/past">
+          <IconButton variant="outline" icon="sandClock" size="lg" />
+        </Link>
       </TooltipButton>
       <TooltipButton context="현재">
-        <IconButton variant="outline" icon="time" size="lg" />
+        <Link href="/present">
+          <IconButton variant="outline" icon="time" size="lg" />
+        </Link>
       </TooltipButton>
       <TooltipButton context="미래">
-        <IconButton variant="outline" icon="rocket" size="lg" />
+        <Link href="/future">
+          <IconButton variant="outline" icon="rocket" size="lg" />
+        </Link>
       </TooltipButton>
     </Container>
   );
