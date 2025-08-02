20a5621073da2ad2ab5caab7f5f83139869d86bf
kwondns <kwondns@gmail.com>
2025-08-02 15:59:46 +0900

    🚀 [Feat]: FutureBoxCard 컴포넌트 추가
    - FutureBoxCard 컴포넌트를 추가하여 미래 작업 박스 카드 UI 구현
      - 제목, 체크박스, 추가 버튼 등의 상단 구조 배치
      - 작업 콘텐츠 렌더링을 위해 FutureContent 컴포넌트 활용
      - 작업이 없는 경우 '작업을 추가 해주세요!' 메시지 출력
    - Card, Typography, Checkbox 등 기존 UI 컴포넌트를 활용하여 재사용성 강화

---
+++ src/organisms/FutureBoxCard.tsx
@@ -0,0 +1,36 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import Typography from '@/atoms/Typography';
+import FutureContent, { FutureContentProps } from '@/molecules/FutureContent';
+import { Checkbox } from '@/components/ui/checkbox';
+import Container from '@/atoms/Container';
+import Icon from '@/atoms/Icon';
+
+export type FutureBoxCardProps = {
+  title: string;
+  id: string;
+  checked: boolean;
+  order: number;
+  futures: FutureContentProps[];
+};
+export default function FutureBoxCard(props: FutureBoxCardProps) {
+  const { title, id, checked, futures } = props;
+  return (
+    <Card className="p-6" id={id}>
+      <CardHeader>
+        <Container className="justify-between">
+          <Typography.h3>{title}</Typography.h3>
+          <Container className="items-center gap-2">
+            <Icon name="add" />
+            <Checkbox checked={checked} />
+          </Container>
+        </Container>
+      </CardHeader>
+      <CardContent className="overflow-x-auto grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] lg:grid-cols-[repeat(auto-fit,_minmax(450px,1fr))] gap-4 p-4">
+        {futures.map((future) => (
+          <FutureContent key={future.id} {...future} />
+        ))}
+        {futures.length < 1 && <Typography.h4 className="pl-2">작업을 추가 해주세요!</Typography.h4>}
+      </CardContent>
+    </Card>
+  );
+}
