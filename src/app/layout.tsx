96cccbb6c9b4f023a9f9d4a576bfd922664b59af
kwondns <kwondns@gmail.com>
2025-08-03 16:33:33 +0900

    🚀 [Feat]: AppSidebar 생성
    - 사이드바 생성 및 적용
    - 각 메뉴를 경로에 맞는 Link 설정
      - `past` -> 과거
      - `calendar` -> 달력
      - `present` -> 현재
      - `future` -> 미래
    - 사이드바 헤더에 Indicator를 추가하여 현재 작업중 여부 파악하기 쉽도록 추가
    - Sidebar가 기본으론 Icon만 보이는 형태 -> hover시 펼쳐져 전체 확인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import './globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import AppSidebar from '@/organisms/AppSidebar';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="bg-background h-dvh">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <div className="p-2 h-dvh">{children}</div>
+          <SidebarProvider className="block" defaultOpen={false}>
+            <AppSidebar />
+            <main className="p-2 pl-18 w-dvw h-dvh">{children}</main>
+          </SidebarProvider>
         </ThemeProvider>
       </body>
     </html>
---
+++ src/organisms/AppSidebar.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from '@/components/ui/sidebar';
+import Link from 'next/link';
+import Typography from '@/atoms/Typography';
+import { CgSandClock, CgCalendar } from 'react-icons/cg';
+import { SlRocket } from 'react-icons/sl';
+import { IoMdTime } from 'react-icons/io';
+import { useEffect, useState } from 'react';
+import { useSelectedLayoutSegments } from 'next/navigation';
+import Indicator from '@/molecules/Indicator';
+
+const Menu: { title: string; url: string; icon: React.ReactNode }[] = [
+  {
+    title: '현재',
+    url: '/present',
+    icon: <IoMdTime />,
+  },
+  {
+    title: '과거',
+    url: '/past',
+    icon: <CgSandClock />,
+  },
+  {
+    title: '달력',
+    url: '/calendar',
+    icon: <CgCalendar />,
+  },
+  {
+    title: '미래',
+    url: '/future',
+    icon: <SlRocket />,
+  },
+];
+
+export default function AppSidebar() {
+  const { setOpen, open } = useSidebar();
+  const [isMounted, setIsMounted] = useState(false);
+  const [transitionClass, setTransitionClass] = useState<string>('opacity-0');
+  const segment = useSelectedLayoutSegments('children');
+
+  useEffect(() => {
+    if (isMounted && open) {
+      // 마운트 후 최소 한 틱 지연
+      requestAnimationFrame(() => {
+        setTransitionClass('opacity-100');
+      });
+    } else {
+      setTransitionClass('opacity-0');
+    }
+  }, [isMounted, open]);
+
+  return (
+    <Sidebar
+      className="fixed"
+      variant="floating"
+      collapsible="icon"
+      onMouseEnter={() => {
+        setOpen(true);
+        setIsMounted(true);
+      }}
+      onMouseLeave={() => {
+        setOpen(false);
+        setIsMounted(false);
+      }}
+    >
+      <SidebarHeader>
+        <SidebarGroup>
+          <SidebarGroupContent className="overflow-x-hidden">
+            <Typography.h4 className="pl-0.5">
+              <Indicator className={`w-12 whitespace-nowrap`} />
+            </Typography.h4>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {Menu.map((menu) => (
+                <SidebarMenuItem key={menu.title}>
+                  <SidebarMenuButton asChild isActive={segment[0] === menu.url.slice(1)}>
+                    <Link href={menu.url}>
+                      {menu.icon}
+                      {open && (
+                        <Typography.p className={`transition-opacity duration-200 ease-in-out ${transitionClass}`}>
+                          {menu.title}
+                        </Typography.p>
+                      )}
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
