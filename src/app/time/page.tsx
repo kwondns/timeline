d555d937d9b1b06e10e3ed2643560af203f72696
kwondns <kwondns@gmail.com>
2025-08-04 21:53:23 +0900

    🚀 [Feat]: Time 페이지 데이터 fetch 기능 추가
    - `getPasts`, `getFutures` 함수 생성하여 API 서버에서 데이터 fetch
      - `/past/count` 및 `/future/analysis` 엔드포인트 호출
    - `Promise.all`로 병렬 처리하여 성능 최적화
    - `TimeTemplate`에 fetch된 `pasts`, `futures` 데이터 전달 및 렌더링

--- src/app/time/page.tsx
+++ src/app/time/page.tsx
@@ -1,6 +1,19 @@
-import { PastMock, FutureMock } from '@/mocks/time.mock';
 import TimeTemplate from '@/templates/Time.template';
+import { TimeFutureType, TimePastType } from '@/types/time.type';
 
-export default function Page() {
-  return <TimeTemplate pasts={PastMock} futures={FutureMock} />;
+async function getPasts(): Promise<TimePastType[]> {
+  const result = await fetch(`${process.env.API_SERVER_URL}/past/count`, { method: 'GET' });
+  return await result.json();
+}
+
+async function getFutures(): Promise<TimeFutureType[]> {
+  const result = await fetch(`${process.env.API_SERVER_URL}/future/analysis`, { method: 'GET' });
+  return await result.json();
+}
+
+export default async function Page() {
+  const pastsData = getPasts();
+  const futuresData = getFutures();
+  const [pasts, futures] = await Promise.all([pastsData, futuresData]);
+  return <TimeTemplate pasts={pasts} futures={futures} />;
 }
