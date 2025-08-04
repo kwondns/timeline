1637705c9337c75f157c8c275b70987c2e0faf92
kwondns <kwondns@gmail.com>
2025-08-04 22:18:16 +0900

    🚀 [Feat]: Calendar 페이지 과거 데이터 API 연동 기능 추가
    - `getPastCalendar` 함수 추가
      - `/past/calendar/{date}` 엔드포인트 호출로 데이터 fetch
      - `PastListType` 타입으로 반환
    - `/calendar` 및 `/calendar/[current]` `Mock` 데이터 삭제 후 API 데이터로 대체
    - `CalendarTemplate`에 fetch된 pasts 데이터 전달 및 렌더링

--- src/app/calendar/[current]/page.tsx
+++ src/app/calendar/[current]/page.tsx
@@ -1,9 +1,11 @@
 import CalendarTemplate from '@/templates/Calendar.template';
-import Mock from '@/mocks/pastCalendar.mock';
+import { getPastCalendar } from '@/app/calendar/page';
 
 export default async function Page({ params }: { params: Promise<{ current: string }> }) {
   const { current: currentString } = await params;
   const current = new Date(currentString);
 
-  return <CalendarTemplate current={current} pasts={Mock}></CalendarTemplate>;
+  const pasts = await getPastCalendar(currentString);
+
+  return <CalendarTemplate current={current} pasts={pasts}></CalendarTemplate>;
 }
--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,9 +1,15 @@
 import CalendarTemplate from '@/templates/Calendar.template';
-import Mock from '@/mocks/pastCalendar.mock';
+import { PastListType } from '@/types/past.type';
+
+export async function getPastCalendar(date: string): Promise<PastListType[]> {
+  const pasts = await fetch(`${process.env.API_SERVER_URL}/past/calendar/${date}`, { method: 'GET' });
+  return pasts.json();
+}
 
 export default async function Page() {
   const currentString = new Date().toISOString().slice(0, 7);
   const current = new Date(currentString);
+  const pasts = await getPastCalendar(currentString);
 
-  return <CalendarTemplate current={current} pasts={Mock}></CalendarTemplate>;
+  return <CalendarTemplate current={current} pasts={pasts}></CalendarTemplate>;
 }
