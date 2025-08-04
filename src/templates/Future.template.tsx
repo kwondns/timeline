d3569bfd5d457b670124960fe3804dd51a2f416f
kwondns <kwondns@gmail.com>
2025-08-04 22:29:04 +0900

    🚑️ [Fix]: FutureTemplate props 및 변수명 오타 수정
    - `FutureTemplateProps`에서 `futureBoxs`를 `futureBoxes`로 수정

--- src/templates/Future.template.tsx
+++ src/templates/Future.template.tsx
@@ -2,15 +2,15 @@ import FutureBoxCard, { FutureBoxCardProps } from '@/organisms/FutureBoxCard';
 import Container from '@/atoms/Container';
 import Typography from '@/atoms/Typography';
 
-export type FutureTemplateProps = { futureBoxs: FutureBoxCardProps[] };
+export type FutureTemplateProps = { futureBoxes: FutureBoxCardProps[] };
 export default function FutureTemplate(props: FutureTemplateProps) {
-  const { futureBoxs } = props;
+  const { futureBoxes } = props;
   return (
     <Container direction="column" className="gap-4 py-8 px-4">
       <Typography.h1 className="self-start">미래 계획</Typography.h1>
       <Typography.h4 className="text-foreground/50 mb-4">목표 설정</Typography.h4>
       <Container direction="column" className="gap-4">
-        {futureBoxs.map((futureBox) => (
+        {futureBoxes.map((futureBox) => (
           <FutureBoxCard key={futureBox.id} {...futureBox} />
         ))}
       </Container>
