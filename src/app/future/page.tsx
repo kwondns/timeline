66893f35fe08a5796f41d4c3f3c92896168a0fb5
kwondns <kwondns@gmail.com>
2025-08-04 22:29:18 +0900

    🚀 [Feat]: Future 페이지 API 연동 기능 추가
    - `getFutureBoxes` 함수 생성
      - `/future` 엔드포인트 호출하여 `FutureBoxCardProps[]` 데이터 fetch
    - `FutureTemplate`에 fetch된 futureBoxes 데이터 전달 및 렌더링
    - 기존 Mock 데이터 삭제 및 API 데이터로 대체

--- src/app/future/page.tsx
+++ src/app/future/page.tsx
@@ -1,6 +1,12 @@
 import FutureTemplate from '@/templates/Future.template';
-import Mock from '@/mocks/future.mock';
+import { FutureBoxCardProps } from '@/organisms/FutureBoxCard';
 
-export default function Page() {
-  return <FutureTemplate futureBoxs={Mock} />;
+async function getFutureBoxes(): Promise<FutureBoxCardProps[]> {
+  const result = await fetch(`${process.env.API_SERVER_URL}/future`, { method: 'GET' });
+  return await result.json();
+}
+
+export default async function Page() {
+  const futureBoxes = await getFutureBoxes();
+  return <FutureTemplate futureBoxes={futureBoxes} />;
 }
