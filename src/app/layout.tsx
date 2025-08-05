f5770672bbfabfa152d1931cf2c97b32a01f5ab4
kwondns <kwondns@gmail.com>
2025-08-05 14:01:46 +0900

    🚀 [Feat]: Toaster 컴포넌트 추가 및림 기능 지원
    - Toaster 컴포넌트 레이아웃에 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import AppSidebar from '@/organisms/AppSidebar';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <SidebarProvider className="block" defaultOpen={false}>
             <AppSidebar />
             <main className="p-2 pl-18 w-dvw h-dvh">{children}</main>
+            <Toaster richColors expand={false} position="top-right" />
           </SidebarProvider>
         </ThemeProvider>
       </body>
