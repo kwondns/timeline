e2471728d494b8a09ce5d2bff0d65a6abd65b251
kwondns <kwondns@gmail.com>
2025-08-06 17:19:50 +0900

    🚀 [Feat]: PresentInfo에 InputOrDisplay 적용
    - `InputOrDisplay` 컴포넌트를 `PresentInfo` 제목 입력에 적용
      - 텍스트 및 입력 상태 동적 전환 가능
      - `title` prop 추가로 초기 값 설정 및 상태 관리
      - 기본 `Typography` 스타일 및 입력 필드 간 스타일 차이 반영

--- src/organisms/PresentInfo.tsx
+++ src/organisms/PresentInfo.tsx
@@ -1,19 +1,20 @@
 'use client';
 
-import { Input } from '@/components/ui/input';
 import Container from '@/atoms/Container';
 import ActionButtonGroup, { ActionButtonGroupProps } from '@/molecules/ActionButtonGroup';
 import { Card } from '@/components/ui/card';
 import Typography from '@/atoms/Typography';
 import { calculateDateDiff } from '@/lib/date';
 import Indicator from '@/molecules/Indicator';
+import InputOrDisplay from '@/atoms/InputOrDisplay';
 
 type PresentInfoProps = {
+  title: string;
   from: Date | null;
 } & ActionButtonGroupProps;
 
 export default function PresentInfo(props: PresentInfoProps) {
-  const { from, onSave, onTempSave } = props;
+  const { from, title, onSave, onTempSave } = props;
   const diff = calculateDateDiff(from ?? '');
   return (
     <Card className="gap-4 px-6 pt-6 bg-info-foreground/20 border-info/20">
@@ -21,8 +22,15 @@ export default function PresentInfo(props: PresentInfoProps) {
         <Indicator active={!!from} />
         <Typography.h4 className="text-primary/70">{from?.toLocaleString('ko-kr')}</Typography.h4>
       </Container>
-      {/* TODO 기본 Typo, 더블클릭 시 Input으로 변경 */}
-      <Input className="!text-2xl !py-6" id="title" placeholder="제목입력" />
+      <InputOrDisplay
+        value={title}
+        typo="h3"
+        className="!text-2xl"
+        inputClassName="!py-6 !px-2"
+        typoClassName="px-2 py-[9px]"
+        id="title"
+        placeholder="제목입력"
+      />
       <Container className="justify-between gap-6 items-center">
         <Typography.h2 className="!pb-0">{diff}</Typography.h2>
         <ActionButtonGroup onTempSave={onTempSave} onSave={onSave} />
