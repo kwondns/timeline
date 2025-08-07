f5424745dc8e332e41d3c0c60b48779a7b7d01f3
kwondns <kwondns@gmail.com>
2025-08-07 15:17:01 +0900

    🚀 [Feat]: 이미지 업로드 및 붙여넣기 기능 추가
    - 드래그 및 붙여넣기 시 이미지 업로드 처리 로직 추가
      - `onImagePasted` 함수 생성 및 데이터 전송 로직 구현
      - 파일 업로드 후 마크다운 형식으로 텍스트 영역에 삽입
    - markdown.ts에 업로드 및 토스트 알림 추가
      - `uploadImageAction`, `toast` 활용한 알림 및 데이터 처리
      - 텍스트 영역 삽입 함수 `insertToTextArea` 구현
    - `updatePresent`에 파일 업로드 및 정리 관련 서버 액션 추가
      - `uploadImageAction` 및 `cleanUpImageAction` 생성
      - 서버와의 통신 및 이미지 정리 로직 통합

--- src/actions/updatePresent.ts
+++ src/actions/updatePresent.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { callFetch } from '@/lib/fetch';
+import { callFetch, fileUpload } from '@/lib/fetch';
 import { revalidatePath } from 'next/cache';
 
 export const updatePresentTitleAction = async (title: string) => {
@@ -13,4 +13,35 @@ export const updatePresentStartAction = async () => {
   await callFetch('/present', { startTime: new Date().toISOString() }, { method: 'PATCH' });
 
   revalidatePath('/present');
+};
+
+export const updatePresentContentAction = async (content: string) => {
+  await callFetch('/present', { content }, { method: 'PATCH' });
+};
+
+type UpdatePresentEndActionType = {
+  startTime: string;
+  endTime: string;
+  content: string;
+  title: string;
+};
+export const updatePresentEndAction = async (payload: UpdatePresentEndActionType) => {
+  await callFetch('/past', payload, { method: 'POST' });
+  revalidatePath('/present');
+};
+
+export const cleanUpImageAction = async (startTime: string) => {
+  const result = await fetch(`${process.env.API_SERVER_URL}/past/cleanup/${startTime}`);
+  return result.json();
+};
+
+type UploadImageActionType = {
+  file: File;
+  uri: string;
+  num: number;
+};
+
+export const uploadImageAction = async (payload: UploadImageActionType) => {
+  const { file, uri, num } = payload;
+  return await fileUpload(file, uri, num);
 };
--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,5 +1,8 @@
+import { ClipboardEvent, DragEvent } from 'react';
 import { Plugin } from 'unified';
 import { visit } from 'unist-util-visit';
+import { uploadImageAction } from '@/actions/updatePresent';
+import { toast } from 'sonner';
 
 type TreeNodeType = {
   tagName: string;
@@ -29,5 +32,61 @@ export const imgLazyLoading: Plugin = () => (tree) => {
     if (node.tagName === 'img' && node.properties) {
       node.properties.loading = 'lazy';
     }
+  });
+};
+
+const insertToTextArea = (insertString: string) => {
+  const textarea = document.querySelector('textarea');
+  if (!textarea) {
+    return null;
+  }
+
+  let sentence = textarea.value;
+  const len = sentence.length;
+  const pos = textarea.selectionStart;
+  const end = textarea.selectionEnd;
+
+  const front = sentence.slice(0, pos);
+  const back = sentence.slice(pos, len);
+
+  sentence = front + insertString + back;
+
+  textarea.value = sentence;
+  textarea.selectionEnd = end + insertString.length;
+
+  return sentence;
+};
+
+export const onImagePasted = async (
+  event: ClipboardEvent<HTMLDivElement> | DragEvent<HTMLDivElement>,
+  dataTransfer: DataTransfer,
+  uri: string,
+  setMarkdown: (value: string) => void,
+) => {
+  const files: File[] = [];
+  for (let index = 0; index < dataTransfer.items.length; index += 1) {
+    const file = dataTransfer.files.item(index);
+
+    if (file) {
+      event.preventDefault();
+      files.push(file);
+      if (uri === '') {
+        toast.error('작업을 먼저 시작해주세요!');
+        return;
+      }
+    }
+  }
+  let result: string[] = [];
+  await Promise.allSettled(
+    files.map(async (file) => {
+      result = await uploadImageAction({ file, num: files.length, uri });
+    }),
+  );
+  result.forEach((route: string) => {
+    const insertedMarkdown = insertToTextArea(`![](${process.env.NEXT_PUBLIC_IMAGE_URL}/${route})`);
+    if (!insertedMarkdown) {
+      return;
+    }
+    setMarkdown(insertedMarkdown);
   });
 };
