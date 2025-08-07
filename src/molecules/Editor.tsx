08277755599815dca85f9f7a7d66f449e4852577
kwondns <kwondns@gmail.com>
2025-08-07 15:17:36 +0900

    🚀 [Feat]: PresentTemplate에 임시 저장 및 최종 저장 기능 추가
    - `onTempSave`, `onSave` 함수 추가
      - `callActionWithToast`를 활용하여 임시 저장 및 최종 저장 로직 구현
      - 저장 성공 및 실패 시 `toast` 알림 처리
    - `Editor`에 핫키(Save: Ctrl+S) 및 이미지 붙여넣기 이벤트 추가
      - `onImagePasted`를 활용해 붙여넣기 및 드래그 시 이미지 업로드 처리
      - `tempSaveAction`으로 임시 저장 동작 연결
    - `Editor`와 `PresentTemplate` 간 props 전달로 상태 관리 통합
      - `setContentAction` 및 `currentContent`로 markdown 편집 상태 유지
      - 저장 시 Props 데이터 최종 반영 및 정리 로직 추가

--- src/molecules/Editor.tsx
+++ src/molecules/Editor.tsx
@@ -1,17 +1,44 @@
 'use client';
 
 import Container from '@/atoms/Container';
+import { onImagePasted } from '@/lib/markdown';
 import MDEditor from '@uiw/react-md-editor';
 
-export default function Editor() {
+type EditorProps = {
+  content: string;
+  setContentAction: (value: string) => void;
+  startTimeString: string;
+  tempSaveAction: () => void;
+};
+
+export default function Editor(props: EditorProps) {
+  const { content, setContentAction, startTimeString, tempSaveAction } = props;
+  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if ((event.metaKey || event.ctrlKey) && event.key === 's') {
+      event.preventDefault();
+      tempSaveAction();
+    }
+  };
   return (
     <Container className="h-full w-full flex-1 overflow-auto pt-4">
       <MDEditor
+        id="content"
         className="flex h-full w-full flex-1 [&_img]:mx-auto [&_img]:!flex [&_img]:max-h-[500px] [&_ol]:list-decimal [&_ul]:list-disc"
         height="100%"
+        value={content}
+        onChange={(value) => {
+          setContentAction(value as string);
+        }}
+        onPaste={async (event) => {
+          await onImagePasted(event, event.clipboardData, startTimeString, setContentAction);
+        }}
+        onDrop={async (event) => {
+          await onImagePasted(event, event.dataTransfer, startTimeString, setContentAction);
+        }}
         textareaProps={{
           placeholder: '꾸준히 작성하자!',
         }}
+        onKeyDown={onKeyDown}
       />
     </Container>
   );
--- src/templates/Present.template.tsx
+++ src/templates/Present.template.tsx
@@ -3,6 +3,10 @@
 import Container from '@/atoms/Container';
 import PresentInfo from '@/organisms/PresentInfo';
 import Editor from '@/molecules/Editor';
+import { useState } from 'react';
+import { cleanUpImageAction, updatePresentContentAction, updatePresentEndAction } from '@/actions/updatePresent';
+import { callActionWithToast } from '@/lib/action';
+import { toast } from 'sonner';
 
 export type PresentTemplateProps = {
   title: string | null;
@@ -11,16 +15,39 @@ export type PresentTemplateProps = {
 };
 
 export default function PresentTemplate(props: PresentTemplateProps) {
-  const { title, startTime } = props;
+  const { title, startTime, content } = props;
+  const [currentContent, setCurrentContent] = useState<string>(content ?? '');
+  const onTempSave = async () => {
+    await callActionWithToast(() => updatePresentContentAction(currentContent));
+  };
+  const onSave = async () => {
+    if (title === null || startTime === null || currentContent === null) return;
+    const loadingToast = toast.loading('기록중...');
+    updatePresentEndAction({ title, startTime, endTime: new Date().toISOString(), content: currentContent })
+      .then(() => {
+        toast.dismiss(loadingToast);
+        toast.success('기록 완료!');
+        cleanUpImageAction(startTime).then((res) => toast.success(`${res}개 스토리지 정리!`));
+      })
+      .catch(() => {
+        toast.dismiss(loadingToast);
+        toast.error('기록 실패!');
+      });
+  };
   return (
     <Container direction="column" className="h-full px-4 pt-8">
       <PresentInfo
         title={title}
         from={startTime ? new Date(startTime) : null}
-        onTempSave={() => {}}
-        onSave={() => {}}
+        onTempSave={onTempSave}
+        onSave={onSave}
       />
-      <Editor />
+      <Editor
+        content={currentContent}
+        startTimeString={startTime ?? ''}
+        setContentAction={setCurrentContent}
+        tempSaveAction={onTempSave}
+      />
     </Container>
   );
 }
