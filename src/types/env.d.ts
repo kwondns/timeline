28418ffa2ec28853155c68072f88432aca364a44
kwondns <kwondns@gmail.com>
2025-08-07 15:17:52 +0900

    🚀 [Feat]: 환경 변수 타입 정의 파일 추가
    - `env.d.ts` 파일 생성 및 환경 변수 타입 정의
      - `process.env` 속성에 `API_SERVER_URL`, `WS_SERVER_URL` 등 추가
      - 클라이언트와 서버 환경 변수 명시적으로 구분 및 타입 안정성 강화

---
+++ src/types/env.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    API_SERVER_URL: string;
+    WS_SERVER_URL: string;
+    NEXT_PUBLIC_IMAGE_URL: string;
+    NEXT_PUBLIC_CHATBOT_URL: string;
+  }
+}
