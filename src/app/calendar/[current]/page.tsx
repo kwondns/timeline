04405c3b1ad81994db83968ff65a9cd230bd0fb3
kwondns <kwondns@gmail.com>
2025-08-08 22:37:29 +0900

    🚀 [Feat]: Calendar 페이지 API 호출 방식 및 SSG/ISR 적용
    - `getPastCalendar` 함수 제거 및 fetch 직접 호출로 변경
    - SSG를 위해 `generateStaticParams` 함수 추가 및 지난 3개월 날짜 생성
    - ISR 재검증 추가:
      - 현재 월: 1시간(3600초)
    - `dynamic` 및 `dynamicParams` 설정 추가로 정적 렌더링 활성화

--- src/app/calendar/[current]/page.tsx
+++ src/app/calendar/[current]/page.tsx
@@ -1,11 +1,32 @@
 import CalendarTemplate from '@/templates/Calendar.template';
-import { getPastCalendar } from '@/app/calendar/page';
+import { PastListType } from '@/types/past.type';
+
+export const dynamic = 'force-static';
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const params: { current: string }[] = [];
+  const today = new Date();
+  for (let i = 1; i <= 3; i++) {
+    const targetDate = new Date(today);
+    targetDate.setMonth(today.getUTCMonth() - i);
+    params.push({ current: targetDate.toISOString().slice(0, 7) });
+  }
+  return params;
+}
 
 export default async function Page({ params }: { params: Promise<{ current: string }> }) {
   const { current: currentString } = await params;
   const current = new Date(currentString);
 
-  const pasts = await getPastCalendar(currentString);
+  const thisMonthString = new Date().toISOString().slice(0, 7);
+
+  const response = await fetch(`${process.env.API_SERVER_URL}/past/calendar/${currentString}`, {
+    method: 'GET',
+    next: { revalidate: thisMonthString === currentString ? 3600 : false },
+  });
+
+  const pasts = (await response.json()) as PastListType[];
 
   return <CalendarTemplate current={current} pasts={pasts}></CalendarTemplate>;
 }
--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,15 +1,14 @@
 import CalendarTemplate from '@/templates/Calendar.template';
 import { PastListType } from '@/types/past.type';
 
-export async function getPastCalendar(date: string): Promise<PastListType[]> {
-  const pasts = await fetch(`${process.env.API_SERVER_URL}/past/calendar/${date}`, { method: 'GET' });
-  return pasts.json();
-}
-
 export default async function Page() {
   const currentString = new Date().toISOString().slice(0, 7);
   const current = new Date(currentString);
-  const pasts = await getPastCalendar(currentString);
+  const response = await fetch(`${process.env.API_SERVER_URL}/past/calendar/${currentString}`, {
+    method: 'GET',
+    next: { revalidate: 3600 },
+  });
+  const pasts = (await response.json()) as PastListType[];
 
   return <CalendarTemplate current={current} pasts={pasts}></CalendarTemplate>;
 }
