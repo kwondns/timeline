7a6a92a48b3c64c40ce8000542ca7702849ce056
kwondns <kwondns@gmail.com>
2025-08-08 23:37:07 +0900

    💄 [Design]: ChatbotTemplate 컴포넌트 레이아웃 방식 수정
    - Container 컴포넌트의 `absolute`를 `fixed`로 변경
    - 화면 스크롤 시 컴포넌트 위치 유지되도록 수정

--- src/templates/Chatbot.template.tsx
+++ src/templates/Chatbot.template.tsx
@@ -8,7 +8,7 @@ import { useState } from 'react';
 export default function ChatbotTemplate() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <Container direction="column" className="absolute bottom-4 right-4 gap-2">
+    <Container direction="column" className="fixed bottom-4 right-4 gap-2">
       <ChatbotArea isOpen={isOpen} />
       <ChatButton isOpen={isOpen} setIsOpenAction={setIsOpen} />
     </Container>
