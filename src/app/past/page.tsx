98edba235f64cf6eaf2081d2cd2224e967bec725
kwondns <kwondns@gmail.com>
2025-08-08 22:38:14 +0900

    🚀 [Feat]: Past 페이지 ISR 재검증 옵션 추가
    - API fetch 옵션에 `next.revalidate` 속성(300초) 추가

--- src/app/past/page.tsx
+++ src/app/past/page.tsx
@@ -3,7 +3,10 @@ import { PastActivityType } from '@/types/past.type';
 
 export default async function Page() {
   const date = new Date().toISOString().slice(0, 10);
-  const response = await fetch(`${process.env.API_SERVER_URL}/past/${date}`, { method: 'GET' });
+  const response = await fetch(`${process.env.API_SERVER_URL}/past/${date}`, {
+    method: 'GET',
+    next: { revalidate: 300 },
+  });
   const activities = (await response.json()) as PastActivityType[];
 
   return <PastTemplate date={date} activities={activities} />;
