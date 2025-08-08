552f1a6fc6c302cba4688de7d95cc01b5912c7d5
kwondns <kwondns@gmail.com>
2025-08-08 15:24:29 +0900

    🚀 [Feat]: 챗봇 관련 Icon 추가
    - Message -> 챗봇 여는 버튼
    - close -> 챗봇 닫는 버튼
    - send -> 채팅 보내는 버튼

--- src/atoms/Icon.tsx
+++ src/atoms/Icon.tsx
@@ -1,9 +1,10 @@
 import { CgSandClock, CgCalendar } from 'react-icons/cg';
 import { SlRocket } from 'react-icons/sl';
-import { IoMdTime, IoMdAdd } from 'react-icons/io';
+import { IoMdTime, IoMdAdd, IoIosClose, IoIosSend } from 'react-icons/io';
 import { MdNavigateNext, MdNavigateBefore } from 'react-icons/md';
 import { RiResetLeftFill } from 'react-icons/ri';
 import { IoAnalytics } from 'react-icons/io5';
+import { TbMessage } from 'react-icons/tb';
 
 export const Icon = {
   sandClock: <CgSandClock />,
@@ -15,6 +16,9 @@ export const Icon = {
   before: <MdNavigateBefore />,
   reset: <RiResetLeftFill />,
   analytics: <IoAnalytics />,
+  message: <TbMessage />,
+  close: <IoIosClose />,
+  send: <IoIosSend />,
 } as const;
 
 export type IconType = keyof typeof Icon;
