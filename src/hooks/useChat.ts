99a1a1f14c296905e9daba2fe752b600020b6b4c
kwondns <kwondns@gmail.com>
2025-08-08 16:00:32 +0900

    🚀 [Feat]: useChat 커스텀 훅 추가
    - 챗봇 호출을 위한 `useChat` 훅 생성
    - SSE 기반 응답 처리 및 실시간 메시지 업데이트
    - `setCurrentChat`, `setIsChatGenerating`, `setChatHistory` 상태 관리 포함

---
+++ src/hooks/useChat.ts
@@ -0,0 +1,45 @@
+import { ChatBubbleProps } from '@/molecules/ChatBubble';
+import { Dispatch, SetStateAction, useCallback } from 'react';
+
+interface UseChatType {
+  setCurrentChat: Dispatch<SetStateAction<string>>;
+  setChatHistory: Dispatch<SetStateAction<ChatBubbleProps[]>>;
+  setIsChatGenerating: Dispatch<SetStateAction<boolean>>;
+}
+const useChat = ({ setCurrentChat, setIsChatGenerating, setChatHistory }: UseChatType) => {
+  const callChat = useCallback(
+    async (payload: { query: string }) => {
+      let responseContent = '';
+      const response = await fetch(`${process.env.NEXT_PUBLIC_CHATBOT_URL}chat`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+          Accept: 'text/event-stream', // SSE 미디어 타입
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.body) return;
+
+      const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+
+      let isDone = false;
+      while (!isDone) {
+        // eslint-disable-next-line no-await-in-loop
+        const { done, value } = await reader.read();
+        if (done) {
+          isDone = done;
+        } else {
+          responseContent += value;
+          setCurrentChat(responseContent);
+        }
+      }
+      setIsChatGenerating(false);
+      setChatHistory((prev) => prev.concat({ isBot: true, text: responseContent }));
+      setCurrentChat('');
+    },
+    [setIsChatGenerating, setChatHistory, setCurrentChat],
+  );
+  return { callChat };
+};
+
+export default useChat;
