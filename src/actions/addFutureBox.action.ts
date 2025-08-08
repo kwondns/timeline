1be3e860be869439b36e2b719495a81556153156
kwondns <kwondns@gmail.com>
2025-08-08 23:39:27 +0900

    🚀 [Feat]: NewFutureBoxDialog 및 addFutureBoxAction 추가
    - `NewFutureBoxDialog` FutureTemplate에 적용하여 UI 향상
    - 미래 계획 추가를 위한 `addFutureBoxAction` 정의
      - `zod`를 활용한 데이터 검증
      - API 호출 및 캐시 경로 재검증 구현
    - 기존 템플릿에서 목표 설정 레이아웃 수정 및 새 대화 상자 연결

---
+++ src/actions/addFutureBox.action.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { callFetch } from '@/lib/fetch';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+
+type AddFutureType = {
+  title: string;
+  type: 'check' | 'progress';
+};
+
+const AddFutureBoxActionSchema = z.object({
+  title: z.string(),
+  type: z.enum(['check', 'progress']),
+});
+
+export const addFutureBoxAction = async (_: any, formData: FormData) => {
+  const title = formData.get('title');
+  const type = formData.get('type');
+  const validatedData = AddFutureBoxActionSchema.parse({ title, type });
+
+  try {
+    await callFetch<AddFutureType>('/future/box', validatedData, { method: 'POST' });
+    revalidatePath('/future');
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
--- src/templates/Future.template.tsx
+++ src/templates/Future.template.tsx
@@ -1,6 +1,7 @@
 import FutureBoxCard, { FutureBoxCardProps } from '@/organisms/FutureBoxCard';
 import Container from '@/atoms/Container';
 import Typography from '@/atoms/Typography';
+import NewFutureBoxDialog from '@/organisms/NewFutureBoxDialog';
 
 export type FutureTemplateProps = { futureBoxes: FutureBoxCardProps[] };
 export default function FutureTemplate(props: FutureTemplateProps) {
@@ -8,7 +9,10 @@ export default function FutureTemplate(props: FutureTemplateProps) {
   return (
     <Container direction="column" className="gap-4 py-8 px-4">
       <Typography.h1 className="self-start">미래 계획</Typography.h1>
-      <Typography.h4 className="text-foreground/50 mb-4">목표 설정</Typography.h4>
+      <Container className="justify-between">
+        <Typography.h4 className="text-foreground/50 mb-4">목표 설정</Typography.h4>
+        <NewFutureBoxDialog />
+      </Container>
       <Container direction="column" className="gap-4">
         {futureBoxes.map((futureBox) => (
           <FutureBoxCard key={futureBox.id} {...futureBox} />
