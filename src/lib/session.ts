7863314c7450c9e49cdeb434d9ebbe998d7a3f01
kwondns <kwondns@gmail.com>
2025-08-10 20:51:36 +0900

    🚀 [Feat]: 세션 검증 시 토큰 자동 갱신 로직 추가
    - `verifySession`:
      - 세션 만료 45분 이전 시 리프레시 토큰 갱신 API 호출
      - `auth-token`, `refresh-token` 쿠키 재설정 및 세션 갱신 처리
    - `AuthResponseType` 타입 적용

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,6 +1,7 @@
 import { JWTPayload, jwtVerify, SignJWT } from 'jose';
 import { cookies } from 'next/headers';
 import { setCookie } from '@/lib/cookie';
+import { AuthResponseType } from '@/types/auth.type';
 
 export interface SessionPayload extends JWTPayload {
   userId: string;
@@ -35,10 +36,26 @@ export async function createSession(userId: string) {
 export async function verifySession() {
   const cookieStore = await cookies();
   const cookie = cookieStore.get('session')?.value;
-  const session = await decrypt(cookie);
+  let session = await decrypt(cookie);
   if (!session?.userId) return null;
 
-  return { isAuth: true, userId: session.userId };
+  const now = Date.now();
+  const timeLeft = session.expiresAt - now;
+  if (timeLeft <= 45 * 60 * 1000) {
+    const refreshResponse = await fetch(`${process.env.API_SERVER_URL}/user/refresh`, {
+      method: 'POST',
+      credentials: 'include',
+    });
+    if (!refreshResponse.ok) return null;
+    const { accessToken, userId, refreshToken } = (await refreshResponse.json()) as AuthResponseType;
+    await createSession(userId);
+    await setCookie('refresh-token', refreshToken, 1000 * 60 * 60 * 24 * 7);
+    await setCookie('auth-token', accessToken, 1000 * 60 * 15);
+    const newCookie = cookieStore.get('session')?.value;
+    session = (await decrypt(newCookie)) as SessionPayload;
+  }
+
+  return { isAuth: true, userId: session.userId, expiresAt: session.expiresAt };
 }
 
 export async function destroySession() {
