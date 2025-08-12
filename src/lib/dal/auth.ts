21d4abaa41317b2139ae8a6c7ce69ee7fe11b62a
kwondns <kwondns@gmail.com>
2025-08-12 17:55:59 +0900

    🚀 [Feat]: 쿠키 기반 인증 검증 및 리다이렉트 로직 추가
    - `validateCookie` 함수 추가:
      - 쿠키에서 `refresh-token` 추출 및 검증
      - 토큰 미존재 시 `/sign/in?toast=loginRequired`로 리다이렉트 처리

--- src/lib/dal/auth.ts
+++ src/lib/dal/auth.ts
@@ -1,4 +1,8 @@
+'use server';
+
 import { AuthResponseType } from '@/types/auth.type';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
 export async function refresh(refreshToken?: string): Promise<null | AuthResponseType> {
   if (!refreshToken) return null;
@@ -12,4 +16,13 @@ export async function refresh(refreshToken?: string): Promise<null | AuthRespons
   });
   if (!res.ok) return null;
   return (await res.json()) as AuthResponseType;
+}
+
+export async function validateCookie() {
+  const cookieStore = await cookies();
+  const refreshToken = cookieStore.get('refresh-token')?.value;
+  if (!refreshToken) {
+    redirect('/sign/in?toast=loginRequired');
+  }
+  return refreshToken;
 }
