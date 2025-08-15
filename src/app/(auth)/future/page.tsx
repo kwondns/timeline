52fd2a65e2736a3547045c03b4fe728923c96d1f
kwondns <kwondns@gmail.com>
2025-08-15 16:12:46 +0900

    ♻️ [Refactor]: Future 페이지 NoFutureBox 템플릿 적용
    - `page.tsx`:
      - 기본 안내 메시지 구조를 `NoFutureBox` 템플릿으로 통일
      - 기존 `Card`, `Typography` 컴포넌트 제거로 코드 간소화
    - `FuturesSlot.tsx`:
      - 데이터가 없는 경우 `NoFutureBox`를 렌더링하도록 로직 수정
      - `NoFutureBox` 이용으로 중복 코드 제거 및 일관성 유지

--- src/app/(auth)/future/page.tsx
+++ src/app/(auth)/future/page.tsx
@@ -1,8 +1,7 @@
 import FutureBoxCard, { FutureBoxCardProps } from '@/organisms/FutureBoxCard';
 import { callGetWithAuth } from '@/lib/dal/http';
 import { headers } from 'next/headers';
-import Typography from '@/atoms/Typography';
-import { Card } from '@/components/ui/card';
+import NoFutureBox from '@/templates/NoFutureBox';
 
 export default async function Page() {
   const userId = (await headers()).get('x-user-id') as string;
@@ -12,8 +11,6 @@ export default async function Page() {
   return futureBoxes.length > 0 ? (
     futureBoxes.map((futureBox) => <FutureBoxCard key={futureBox.id} {...futureBox} />)
   ) : (
-    <Card className="p-6 max-h-[450px]">
-      <Typography.span>계획을 추가 해주세요!</Typography.span>
-    </Card>
+    <NoFutureBox />
   );
 }
--- src/app/(auth)/time/_slot/FuturesSlot.tsx
+++ src/app/(auth)/time/_slot/FuturesSlot.tsx
@@ -1,10 +1,12 @@
 import TimeFutureProgress from '@/organisms/TimeFutureProgress';
 import { headers } from 'next/headers';
 import { getFutures } from '@/app/(auth)/time/_api/getFutures';
+import NoFutureBox from '@/templates/NoFutureBox';
 
 export default async function FuturesSlot() {
   const userId = (await headers()).get('x-user-id') as string;
   const futures = await getFutures(userId);
 
-  return futures.map((future) => <TimeFutureProgress key={future.id} futureBox={future} />);
+  if (futures.length > 0) futures.map((future) => <TimeFutureProgress key={future.id} futureBox={future} />);
+  return <NoFutureBox />;
 }
