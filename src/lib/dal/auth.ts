3107a3410585d8870ae6ccd17bf656f95b293b2c
kwondns <kwondns@gmail.com>
2025-08-16 20:16:07 +0900

    ♻️ [Refactor]: 사용되지 않는 함수 제거
    - `auth.ts`:
      - 더 이상 참조되지 않는 `tryRefreshSessionOnce` 함수 삭제
    - 관련된 로직이 이미 다른 방식으로 처리되어 불필요한 코드 제거

--- src/lib/dal/auth.ts
+++ src/lib/dal/auth.ts
@@ -3,7 +3,6 @@
 import { AuthResponseType } from '@/types/auth.type';
 import { cookies } from 'next/headers';
 import { NoRefreshTokenError } from '@/lib/error';
-import { refreshSession } from '@/lib/session';
 
 export async function refresh(refreshToken?: string): Promise<null | AuthResponseType> {
   if (!refreshToken) return null;
@@ -26,17 +25,4 @@ export async function validateRefreshToken() {
     throw new NoRefreshTokenError();
   }
   return refreshToken;
-}
-
-async function tryRefreshSessionOnce(): Promise<boolean> {
-  try {
-    const refreshToken = await validateRefreshToken();
-    const newSession = await refresh(refreshToken);
-    if (!newSession) return false;
-    await refreshSession(newSession);
-    return true;
-  } catch (e) {
-    if (e instanceof NoRefreshTokenError) return false;
-    throw e;
-  }
 }
