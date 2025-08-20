ccca50b424baf610c293bcb17fe97101b90d4eb2
kwondns <kwondns@gmail.com>
2025-08-21 02:02:43 +0900

    🚑️ [Fix]: past, calender redirect 수정
    - 현재 날짜를 KST 시간대로 변경하여 정확한 날짜로 이동

--- src/app/(auth)/calendar/page.tsx
+++ src/app/(auth)/calendar/page.tsx
@@ -3,5 +3,7 @@ import { redirect } from 'next/navigation';
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  redirect(`/calendar/${new Date().toISOString().slice(0, 7)}`);
+  const current = new Date(new Date().getTime() + 9 * 60 * 60 * 1000).toISOString().slice(0, 7);
+
+  redirect(`/calendar/${current}`);
 }
--- src/app/(auth)/past/page.tsx
+++ src/app/(auth)/past/page.tsx
@@ -3,6 +3,6 @@ import { redirect } from 'next/navigation';
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const date = new Date().toISOString().slice(0, 10);
+  const date = new Date(new Date().getTime() + 9 * 60 * 60 * 1000).toISOString().slice(0, 10);
   redirect(`/past/${date}`);
 }
