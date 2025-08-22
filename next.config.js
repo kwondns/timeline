17e72b0030aedc394a11f4c9c5bbc1e3d82b2bdc
kwondns <kwondns@gmail.com>
2025-08-22 20:17:47 +0900

    🔨 [Chore]: next-intl 플러그인 추가 및 다국어 지원 설정
    - `next-intl` 패키지 의존성 추가
    - `next-config.js`에 `next-intl` 플러그인 설정으로 다국어 지원 활성화
    - `yarn.lock` 갱신으로 관련 의존성 업데이트

--- next.config.js
+++ next.config.js
@@ -1,62 +1,67 @@
+const createNextIntlPlugin = require('next-intl/plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+
+const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_IMAGE_URL,
-      },
-    ],
-  },
-  experimental: {
-    ppr: 'incremental',
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // 번들 분할 최적화
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-            priority: 10,
-          },
-          editor: {
-            test: /[\\/]node_modules[\\/](react-md-editor|@uiw|codemirror)[\\/]/,
-            name: 'editor',
-            chunks: 'all',
-            priority: 20,
-          },
-          ui: {
-            test: /[\\/]node_modules[\\/](@radix-ui|lucide-react|next-themes)[\\/]/,
-            name: 'ui-libs',
-            chunks: 'all',
-            priority: 15,
-          },
-          common: {
-            test: /[\\/]src[\\/](atoms|molecules|templates|components|lib)[\\/]/,
-            name: 'common',
-            minChunks: 2,
-            chunks: 'all',
-            priority: 5,
-          },
-          timeline: {
-            test: /[\\/]src[\\/]app[\\/]\(auth\)[\\/](present|past|future|calendar|time)[\\/]/,
-            name: 'timeline-pages',
-            minChunks: 1,
-            chunks: 'all',
-            priority: 8,
-          },
+const nextConfig = withBundleAnalyzer(
+  withNextIntl({
+    images: {
+      formats: ['image/avif', 'image/webp'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: process.env.NEXT_PUBLIC_IMAGE_URL,
         },
-      };
-    }
-    return config;
-  },
-});
+      ],
+    },
+    experimental: {
+      ppr: 'incremental',
+    },
+    webpack: (config, { dev, isServer }) => {
+      if (!dev && !isServer) {
+        // 번들 분할 최적화
+        config.optimization.splitChunks = {
+          chunks: 'all',
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all',
+              priority: 10,
+            },
+            editor: {
+              test: /[\\/]node_modules[\\/](react-md-editor|@uiw|codemirror)[\\/]/,
+              name: 'editor',
+              chunks: 'all',
+              priority: 20,
+            },
+            ui: {
+              test: /[\\/]node_modules[\\/](@radix-ui|lucide-react|next-themes)[\\/]/,
+              name: 'ui-libs',
+              chunks: 'all',
+              priority: 15,
+            },
+            common: {
+              test: /[\\/]src[\\/](atoms|molecules|templates|components|lib)[\\/]/,
+              name: 'common',
+              minChunks: 2,
+              chunks: 'all',
+              priority: 5,
+            },
+            timeline: {
+              test: /[\\/]src[\\/]app[\\/]\(auth\)[\\/](present|past|future|calendar|time)[\\/]/,
+              name: 'timeline-pages',
+              minChunks: 1,
+              chunks: 'all',
+              priority: 8,
+            },
+          },
+        };
+      }
+      return config;
+    },
+  }),
+);
 module.exports = nextConfig;
