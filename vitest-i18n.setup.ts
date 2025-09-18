cd68ae8571f22bce576342c5b0e9773b08d9e496
kwondns <kwondns@gmail.com>
2025-09-18 17:15:33 +0900

    🚀 [Feat]: next-intl 모킹
    - vitest 최상위에서 vi.mock을 활용한 next-intl 자체 모킹
    - next-intl/server
      - 서버 컴포넌트에서 사용시 getTranslation 모킹
    - next-intl
      - 클라이언트 컴포넌트에서 사용하는 useTranslations 모킹
    - vitest.config에 등록

---
+++ vitest-i18n.setup.ts
@@ -0,0 +1,54 @@
+import { Locale } from '@/i18n/routing';
+import ko from './messages/ko.json';
+import en from './messages/en.json';
+import ja from './messages/ja.json';
+import es from './messages/es.json';
+import fr from './messages/fr.json';
+import zhCN from './messages/zh-cn.json';
+import { vi } from 'vitest';
+
+const messagesObject: Record<Locale, any> = {
+  ko,
+  en,
+  ja,
+  es,
+  fr,
+  'zh-cn': zhCN,
+};
+
+const findMessage = (key: string, messageTree: Record<string, Record<string, string> | string>) => {
+  if (key.includes('.')) {
+    const k = key.split('.');
+    const target = k.shift() ?? '';
+    return findMessage(k.join('.'), messageTree[target] as Record<string, string>);
+  }
+  return messageTree[key];
+};
+
+export const createTranslations = (locale: Locale, namespace: string) => {
+  let messages: Record<string, string> = {};
+  if (namespace === '') messages = messagesObject[locale] ?? {};
+  else messages = messagesObject[locale]?.[namespace] ?? {};
+  return (key: string) => {
+    return findMessage(key, messages);
+  };
+};
+
+vi.mock('next-intl/server', () => {
+  return {
+    getTranslations: (ns?: string) => {
+      const locale: Locale = (global as any).__TEST_LOCALE__ ?? 'ko';
+      return createTranslations(locale, ns ?? '');
+    },
+  };
+});
+
+vi.mock('next-intl', async (defaultImport) => {
+  return {
+    ...defaultImport,
+    useTranslations: vi.fn((ns?: string) => {
+      const locale: Locale = (global as any).__TEST_LOCALE__ ?? 'ko';
+      return createTranslations(locale, ns ?? '');
+    }),
+  };
+});
